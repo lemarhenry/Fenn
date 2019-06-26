6c3e56c8798fa4fd450219a25773da804c63def2
lukarrio1 <tomennis1997@gmail.com>
2019-06-26 17:50:20 -0500

    view all status update

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -26,8 +26,7 @@
 </head>
 <body>
     <div id="app">
-        @include('inc.navbar')
-        <main class="py-4">
+        <main class="py-4 bg-dark">
             @yield('content')
         </main>
         <!-- JQuery -->
