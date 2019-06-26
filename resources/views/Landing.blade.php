714c46ff9b77ad47166d68b9159397023107568f
ProfHenry <lemarhenry97.gmail.com>
2019-06-26 18:47:10 -0500

    home

--- resources/views/Landing.blade.php
+++ resources/views/Landing.blade.php
@@ -3,20 +3,16 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-6 pb-3 pr-0">
-            <div class="card card-image" style="background-image: url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg);">
+            <div class="card card-image" style="background-image: url(https://www.wallpaperup.com/1089009/backlight_black_close-up_computer_connection_device_display_electronics_equipment_keyboard_laptop_letters_light_modern_office_portable_screen_technology_white.html);">
                 <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
-                  
+
                   <div>
                     <h5 class="green-text "><i class="fas fa-home"></i></i>Home</h5>
                     <h3 class="card-title pt-2"><strong>This is the card title</strong></h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
                       optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos.
                       Odit sed qui, dolorum!.</p>
-<<<<<<< HEAD
-                    <a class="btn btn-green"><i class="fas fa-home left"></i> Home</a>
-=======
-                    <a class="btn btn-pink" href="/home"><i class="fas fa-clone left"></i> View project</a>
->>>>>>> db60ed085a418c36d84b06f2a5006834584730e1
+                    <a class="btn btn-green" href="/home"><i class="fas fa-home left"></i> Home</a>
                   </div>
                 </div>
 
