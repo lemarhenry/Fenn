c690ef7fcd96bd3a253c131d0f22735c884d10ae
ProfHenry <lemarhenry97.gmail.com>
2019-06-26 17:29:43 -0500

    added landing card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,107 +1,17 @@
-@extends('layouts.app')
-@section('content')
-<div class="row">
+<!-- Card -->
+<div class="card card-image" style="background-image: url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg);">
 
-    <!-- Grid column -->
-    <div class="col-lg-4 col-md-12">
-
-      <!--Card-->
-      <div class="card">
-
-        <!--Card image-->
-        <div class="view">
-          <img src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg" class="card-img-top"
-            alt="photo">
-          <a href="#">
-            <div class="mask rgba-white-slight"></div>
-          </a>
-        </div>
-
-        <!--Card content-->
-        <div class="card-body">
-          <!--Title-->
-          <h4 class="card-title">Card title</h4>
-          <!--Text-->
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-            card's content.</p>
-          <a href="#" class="btn btn-primary">Button</a>
-        </div>
-
+    <!-- Content -->
+    <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
+      <div>
+        <h5 class="pink-text"><i class="fas fa-chart-pie"></i> Marketing</h5>
+        <h3 class="card-title pt-2"><strong>This is the card title</strong></h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
+          optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos.
+          Odit sed qui, dolorum!.</p>
+        <a class="btn btn-pink"><i class="fas fa-clone left"></i> View project</a>
       </div>
-      <!--/.Card-->
-
     </div>
-    <!-- Grid column -->
-
-    <!-- Grid column -->
-    <div class="col-lg-4 col-md-6">
-
-      <!--Card-->
-      <div class="card">
-
-        <!--Card image-->
-        <div class="view">
-          <img src="https://mdbootstrap.com/img/Photos/SLides/img%20(125).jpg" class="card-img-top" alt="photo">
-          <a href="#">
-            <div class="mask rgba-white-slight"></div>
-          </a>
-        </div>
-
-        <!--Card content-->
-        <div class="card-body elegant-color white-text">
-          <!--Title-->
-          <h4 class="card-title">Title of the news</h4>
-          <!--Text-->
-          <p class="card-text white-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et
-            quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas.</p>
-          <a href="#" class="btn btn-outline-white btn-md waves-effect">Button</a>
-        </div>
-
-      </div>
-      <!--/.Card-->
-
-    </div>
-    <!-- Grid column -->
-
-    <!-- Grid column -->
-    <div class="col-lg-4 col-md-6">
-
-      <!--Panel-->
-      <div class="card">
-        <h3 class="card-header light-blue lighten-1 white-text text-uppercase font-weight-bold text-center py-5">Features
-          List</h3>
-        <div class="card-body">
-          <ul class="list-group">
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-              Cras justo odio
-              <span class="badge badge-primary badge-pill">14</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-              Dapibus ac facilisis in
-              <span class="badge badge-primary badge-pill">2</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-              Morbi leo risus
-              <span class="badge badge-primary badge-pill">1</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-              Cras justo odio
-              <span class="badge badge-primary badge-pill">14</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-              Dapibus ac facilisis in
-              <span class="badge badge-primary badge-pill">2</span>
-            </li>
-          </ul>
-          <p class="text-small text-muted mb-0 pt-3">* At vero eos et accusamus et iusto ducimus.</p>
-        </div>
-      </div>
-      <!--/.Panel-->
-
-    </div>
-    <!-- Grid column -->
 
   </div>
-  <!-- Grid row -->
-  @endsection
+  <!-- Card -->
