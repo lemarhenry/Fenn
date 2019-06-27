c35c9004d3d52a8d0aa0e08cbf660f759d4dc2c0
MrAzharEwers <30994816+MrAzharEwers@users.noreply.github.com>
2019-06-27 12:59:31 -0500

    Minor Changes
    Landing
    Networking

---
+++ resources/views/Networking.blade.php
@@ -0,0 +1,232 @@
+@extends('layouts.networklayout')
+@section('content')
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Fenn</title>
+
+</head>
+
+<body>
+
+  <!--Main navigation-->
+  <header>
+
+  </header>
+  <!--Main navigation-->
+  <!--Main Layout-->
+  <main class="mt-5">
+<!--Main Container-->
+<div class="container">
+  <!--Grid Row-->
+  <div class="row">
+    <!--Grid Column-->
+<div class="col-md-7 mb-5">
+<div class="view overlay hm-white-light z-depth-1-half">
+    <img src="img/h.jpg" class="img-fluid" alt="road">
+    <div class="mask"></div>
+</div>
+</div>
+<div class="col-md-5 mb-5">
+  <h2>Networking and Cabling</h2>
+  <hr>
+  <p>Ahh here we go again another one these guys but what can you about it</p>
+<a href="#" class="
+btn btn-indigo">You Ready For This</a>
+</div>
+  </div>
+ <br>
+  <!--Grid Row-->
+  <div class="row">
+  <div class="col-lg-4 col-md-12 mb-4">
+  <!-- Card -->
+<div class="card">
+
+    <!-- Card image -->
+    <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+
+    <!-- Card content -->
+    <div class="card-body">
+
+      <!-- Title -->
+      <h4 class="card-title"><a>Networking Supplies</a></h4>
+      <!-- Text -->
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <!-- Button -->
+      <a href="#" class="btn btn-indigo">Button</a>
+
+    </div>
+
+  </div>
+  <!-- Card -->
+    </div>
+
+
+    <div class="col-lg-4 col-md-3 mb-4">
+  <!-- Card -->
+<div class="card">
+
+    <!-- Card image -->
+    <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+
+    <!-- Card content -->
+    <div class="card-body">
+
+      <!-- Title -->
+      <h4 class="card-title"><a>Cables</a></h4>
+      <!-- Text -->
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <!-- Button -->
+      <a href="#" class="btn btn-indigo">Button</a>
+
+    </div>
+
+  </div>
+  <!-- Card -->
+      </div>
+
+      <div class="col-lg-4 col-md-3 mb-4">
+ <!-- Card -->
+<div class="card">
+
+    <!-- Card image -->
+    <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+
+    <!-- Card content -->
+    <div class="card-body">
+
+      <!-- Title -->
+      <h4 class="card-title"><a>Networking Services</a></h4>
+      <!-- Text -->
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <!-- Button -->
+      <a href="#" class="btn btn-indigo">Button</a>
+
+    </div>
+
+  </div>
+  <!-- Card -->
+        </div>
+         <!-- Card -->
+
+         <div class="col-lg-4 col-md-3 mb-4">
+                <!-- Card -->
+               <div class="card">
+
+                   <!-- Card image -->
+                   <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+
+                   <!-- Card content -->
+                   <div class="card-body">
+
+                     <!-- Title -->
+                     <h4 class="card-title"><a>Networking Services</a></h4>
+                     <!-- Text -->
+                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                     <!-- Button -->
+                     <a href="#" class="btn btn-indigo">Button</a>
+
+                   </div>
+
+                 </div>
+                 <!-- Card -->
+                       </div>
+      </div>
+
+      </div>
+</div>
+  </main>
+  <!--Main Layout-->
+  <!--Footer-->
+  <footer>
+      <!-- Footer -->
+<footer class="page-footer font-small indigo pt-4 text-center">
+
+    <!-- Footer Links -->
+    <div class="container">
+
+      <!-- Grid row -->
+      <div class="row">
+
+        <!-- Grid column -->
+        <div class="col-md-6 mt-md-0 mt-3 ">
+
+          <!-- Content -->
+          <h5 class="text-uppercase">Footer Content</h5>
+          <p>Here you can use rows and columns to organize your footer content.</p>
+
+        </div>
+        <!-- Grid column -->
+
+        <hr class="clearfix w-100 d-md-none pb-3">
+
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3">
+
+          <!-- Links -->
+          <h5 class="text-uppercase">Links</h5>
+
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!">Link 1</a>
+            </li>
+            <li>
+              <a href="#!">Link 2</a>
+            </li>
+            <li>
+              <a href="#!">Link 3</a>
+            </li>
+            <li>
+              <a href="#!">Link 4</a>
+            </li>
+          </ul>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3">
+
+          <!-- Links -->
+          <h5 class="text-uppercase">Links</h5>
+
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!">Link 1</a>
+            </li>
+            <li>
+              <a href="#!">Link 2</a>
+            </li>
+            <li>
+              <a href="#!">Link 3</a>
+            </li>
+            <li>
+              <a href="#!">Link 4</a>
+            </li>
+          </ul>
+
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+      <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+
+
+  <!-- Footer -->
+  </footer>
+
+
+@endsection
