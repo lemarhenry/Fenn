7f86fa1fb33836122914b27857a9937ea0624686
ProfHenry <lemarhenry97.gmail.com>
2019-06-28 00:52:26 -0500

    added Cms files

--- resources/views/Landing.blade.php
+++ resources/views/Landing.blade.php
@@ -28,11 +28,11 @@
             <div class="card card-image" style="background-image: url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg);">
                 <div class="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
                   <div>
-                    <h5 class="pink-text"><i class="fas fa-chart-pie"></i> Marketing</h5>
+                    <h5 class="pink-text"><i class="far fa-building left"></i>Corporate</h5>
                     {{-- <h3 class="card-title pt-2"><strong>This is the card title</strong></h3> --}}
                     <p> FennTech Ltd is an IT solutions provider with over 14 years experience in crafting solutions for home, office, schools and government.
                             The company was founded on the premiss that you don’t just need a computer, nor a fax machine, but a solution for all your technology needs.</p>
-                    <a class="btn btn-pink"><i class="fas fa-clone left"></i> View project</a>
+                    <a class="btn btn-pink"><i class="far fa-building left"></i>Corporate</a>
                   </div>
                 </div>
 
